af1f30bf9509f82c128c10b5d081e9f740c62f39
Koulini <ragikoulinireddy@gmail.com>
2026-04-20 21:45:29 +0530

    Fix Resume folder submodule issue

--- Resume/src/main/java/com/snist/Resume/repository/ResumeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.snist.Resume.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.snist.Resume.model.Resume;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
